66b3aeefddde4fc29f9450402c12b94304e83b8e
Bridget Akellian <akellianbv@gmail.com>
2022-01-10 16:51:29 -0500

    commit tree 48f1c90d971d27c3c6518a55aed582aefdafe0bb

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(x, y) {
+    return x + y;
+  }
+
+function subtract(x, y) {
+    return x - y;
+  }
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+number = 746;
+
+function increment(n) {
+    return n + 1;
+}
+
+function decrement(n) {
+    return n - 1;
+}
+
+
